b051b6ba9a0c901747dec4bd96e79ee6d85adf8b
Md. Sajib Ahmed <asajib499@gmail.com>
2025-06-26 18:16:05 +0600

    refactor: comment out unused fetch and data mapping in homePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 const homePage = async () => {
-  const response = await fetch("https://yotpo.vercel.app/api/posts", {
-    next: { revalidate: 60 }, // Revalidate every 60 seconds
-  });
+  // const response = await fetch("https://yotpo.vercel.app/api/posts", {
+  //   next: { revalidate: 60 }, // Revalidate every 60 seconds
+  // });
 
-  const data = await response.json();
+  // const data = await response.json();
 
   return (
     <div>
       <h1> Home page</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.map((post: { id: number; title: string }) => (
           <div key={post.id}>
             <h2>{post.title}</h2>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
